bddb77735bc153cccfdd791d28ea323c709e8742
Ahess96 <austinhess1996@comcast.net>
2023-05-06 08:19:38 -0700

    Connect to DB and create wireframe

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  const [user, setUser] = useState(null)
+
+  return (
+    <div className="app">
+      Xplor
+    </div>
+  )
+}
+
+export default App
