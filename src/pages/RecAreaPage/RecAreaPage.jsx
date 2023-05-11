1ab7145139622ebc0c1f4de03e4d40ea14036cdb
Ahess96 <austinhess1996@comcast.net>
2023-05-11 16:39:54 -0700

    Conditionally render recAreas

--- src/pages/RecAreaPage/RecAreaPage.jsx
+++ src/pages/RecAreaPage/RecAreaPage.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 import RecAreaDetails from '../../components/RecAreaDetails/RecAreaDetails'
 
 
-const ITEMS_PER_PAGE = 24
+const ITEMS_PER_PAGE = 19
 
 export default function RecAreaPage({activeRecArea, selectActiveRecArea}) {
   
@@ -43,27 +43,62 @@ export default function RecAreaPage({activeRecArea, selectActiveRecArea}) {
 
   return (
     <>
-      <form onSubmit={(evt) => handleSearch(evt)}>
-        <input type="text" placeholder='Search Rec Areas' onChange={handleChange} value={searchInput} name='search' />
-        <button type='submit' name='search'>Search</button> 
-      </form>
+      <div className='mt-12'>
+        <h2 className='text-center'>Search Keywords to Find Recreation Areas</h2>
+      </div>
+
+      {/* Search Bar */}
       
-      {/* <ul> */}
+      <div className='flex justify-center items-center mt-36 mb-12'>
+        <form onSubmit={(evt) => handleSearch(evt)} className="w-full md:w-1/4 mt-5">
+          <input
+            type="text"
+            placeholder='e.g. california hike'
+            onChange={handleChange}
+            value={searchInput}
+            name='search'
+            className='border-2 border-orange-700 rounded-md bg-white rounded-lg px-4 py-2'
+          />
+          <button
+            type='submit'
+            name='search'
+            className='bg-gray-800 text-white rounded-lg px-4 py-2 ml-6'
+          >
+            Search
+          </button>
+        </form>
+      </div>
+
         {/* if recAreas are found, list them otherwise show message */}
         { recAreas.length ?
         <>
         { !activeRecArea ?
           <>
-            <ul>
+            <div className="flex flex-wrap justify-center">    
             {recAreas.map((recArea, idx) => (
                 recArea.RecAreaName.length ?
-                    <li key={idx} onClick={() => selectActiveRecArea(recArea)}>
-                    {recArea.RecAreaName}
-                    </li>
+                  <div key={idx} onClick={() => selectActiveRecArea(recArea)}                  
+                  className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-2 mb-4'
+                  >
+                    {recArea.Keywords ? 
+                    <div className='bg-slate-200 h-32 rounded-lg p-4'>
+                    <p>
+                      {recArea.RecAreaName}
+                    </p>
+                    </div>     
+                    :
+                    <div className='bg-amber-200 h-32 rounded-lg p-4'>
+                      <p>
+                        {recArea.RecAreaName}
+                      </p>
+                    </div>     
+                    }
+                                 
+                  </div>
                     :
                     null  
             ))}
-            </ul>
+            </div>
 
             {Array.from(new Array(pageCount || 1), (_, i) => i + 1).map((num, idx) => {
                 if (num === page) {
@@ -77,9 +112,10 @@ export default function RecAreaPage({activeRecArea, selectActiveRecArea}) {
         }
         </>
         :
-        <p>Make a valid search to see results!</p>
+        <div className=''>
+          <p className='text-center'>Make a valid search to see results!</p>
+        </div>
         }
-      {/* </ul> */}
     </>
   )
 }
